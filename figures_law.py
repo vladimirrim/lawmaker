6c0c507bb888db8a9c504b3f51028be38407890e
vladimirrim <vladimirrim98@gmail.com>
2019-04-14 20:07:36 +0300

    Setup zero for remote.
    Former-commit-id: f244c6214dbf4a6076606ee408c9117f48994bcf
    Former-commit-id: b8a4837651ece84bdf10c0e8c5d42c98156706de

--- figures_law.py
+++ figures_law.py
@@ -5,10 +5,16 @@ if __name__ == '__main__':
     ROOT = 'resource/exps/laws/'
     with open(ROOT + 'actions.txt', 'r') as f:
         actions = [[int(x) for x in line.split()] for line in f]
-        steps = range(len(actions))
+        steps = range(len(actions) // 8)
         punishments = [[] for _ in range(9)]
 
-        for action in actions:
+        for j in range(0, len(actions) - 8, 8):
+            action = [0.] * 8
+            for i in range(8):
+                for nn in range(8):
+                    action[i] += actions[j + nn][i]
+                action[i] /= 8
+
             avg = 0
             for i in range(8):
                 avg += action[i]
