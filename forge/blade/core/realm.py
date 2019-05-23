b23a0f1cb3687c4fbb119978d33c9f4311d67916
vladimirrim <vladimirrim98@gmail.com>
2019-05-24 00:50:22 +0300

    Setup atalanta 2.0 for remote.

--- forge/blade/core/realm.py
+++ forge/blade/core/realm.py
@@ -169,7 +169,7 @@ class NativeRealm(Realm):
                 continue
 
             stim = self.getStim(ent)
-            action, arguments, val = self.sword.decide(ent, stim, self.currentAction)
+            action, arguments, val = self.sword.decide(ent, stim, self.currentAction, self.stepCount)
             ent.act(self.world, action, arguments, val)
 
             self.stepEnt(ent, action, arguments)
