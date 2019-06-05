b6cce41ec37d2a22927e1446800bb805ffe0e7c7
vladimirrim <vladimirrim98@gmail.com>
2019-06-06 02:17:54 +0300

    Fix multiproccessing, add stable version.

--- forge/trinity/smith.py
+++ forge/trinity/smith.py
@@ -3,6 +3,9 @@ import ray
 
 from forge.blade import core, lib
 from forge.trinity.ann import Lawmaker
+import numpy as np
+
+
 # from forge.blade.entity.lawmaker.atalanta.atalanta import Atalanta
 
 # Wrapper for remote async multi environments (realms)
@@ -20,13 +23,11 @@ class NativeServer:
         return ray.get(recvs)
 
     # Use native api (runs full trajectories)
-    def run(self, swordUpdate=None):
-        recvs = [e.run.remote(swordUpdate) for e in self.envs]
-        recvs = ray.get(recvs)
-        return recvs
-        # recvs = np.array(ray.get(recvs))
-        # return [(recvs[i][0], recvs[i][1]) for i in range(len(self.envs))],\
-              # np.mean(recvs[:, 2]), np.mean(recvs[:, 3])
+    def run(self, swordUpdate=None, lawmaker=None):
+        recvs = [e.run.remote(swordUpdate, lawmaker) for e in self.envs]
+        recvs = np.array(ray.get(recvs))
+        return [(recvs[i][0], recvs[i][1]) for i in range(len(self.envs))], recvs[:, 2]
+        # np.mean(recvs[:, 2]), np.mean(recvs[:, 3])
 
     def send(self, swordUpdate):
         [e.recvSwordUpdate.remote(swordUpdate) for e in self.envs]
@@ -56,14 +57,16 @@ class Native(Blacksmith):
         self.trinity = trinity
         self.stepCount = 0
         self.period = 1
+        self.nRealm = args.nRealm
 
         self.env = NativeServer(config, args, trinity)
         self.env.send(self.pantheon.model)
 
-        #self.statsCollector = Demeter(config.NPOP, args.nRealm)
-        #self.lawmaker = Atalanta(self.statsCollector.featureSize)
+        # self.statsCollector = Demeter(config.NPOP, args.nRealm)
+        # self.lawmaker = Atalanta(self.statsCollector.featureSize)
         # self.lawmaker.load('checkpoints/atalanta')
 
+        self.lawmaker = None
         self.renderStep = self.step
         self.idx = 0
 
@@ -71,11 +74,14 @@ class Native(Blacksmith):
     # With no communication -- all on the env cores.
     def run(self):
         self.stepCount += 1
-        recvs = self.env.run(self.pantheon.model)  # , states, rewards
+        recvs, lawmakers = self.env.run(self.pantheon.model, self.lawmaker)  # , states, rewards
+        self.lawmaker = None
 
-        ### sometimes update lawmaker here
+        self.env.lawmaker.gatherStatistics(lawmakers)
+
         if self.env.lawmaker.count > self.env.lawmaker.update_period:
             self.env.lawmaker.backward()
+            self.lawmaker = self.env.lawmaker
 
         self.pantheon.step(recvs)
         self.rayBuffers()
