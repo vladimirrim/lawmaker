59156efd0fc79d9f3cf27e6edb6b807021efcce6
vladimirrim <vladimirrim98@gmail.com>
2019-06-16 17:21:35 +0300

    Deleting old logs.

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -1,26 +1,27 @@
+#!/usr/bin/env bash
 #Pytorch
-pip3.6 install http://download.pytorch.org/whl/cu90/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
-pip3.6 install torchvision
+pip3 install http://download.pytorch.org/whl/cu90/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
+pip3 install torchvision
 
 #Ray -- will segfault in .backward with wrong version
 #pip uninstall -y ray
 #pip install -U https://s3-us-west-2.amazonaws.com/ray-wheels/68b11c8251dc41e79dc9dc4b1da31630708a6bd8/ray-0.4.0-cp36-cp36m-manylinux1_x86_64.whl
 
 #Ray
-pip3.6 install ray
-pip3.6 install setproctitle
-pip3.6 install service_identity
+pip3 install ray
+pip3 install setproctitle
+pip3 install service_identity
 
 #Basics
-pip3.6 install numpy
-pip3.6 install scipy
-pip3.6 install matplotlib
+pip3 install numpy
+pip3 install scipy
+pip3 install matplotlib
 
 
 #Tiled map loader
-pip install pytmx
+pip3 install pytmx
 
 #jsonpickle
-pip install jsonpickle
-pip install opensimplex
-pip install twisted
+pip3 install jsonpickle
+pip3 install opensimplex
+pip3 install twisted
